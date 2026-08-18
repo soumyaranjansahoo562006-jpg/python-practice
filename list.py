3f0db7a360d97b154fb4bf480c8b3e537ea93376
soumyaranjansahoo562006-jpg <soumyaranjansahoo562006@gmail.com>
2026-08-18 10:50:10 +0530

    6th change

--- list.py
+++ list.py
@@ -1,10 +1,8 @@
-movies=[]
-movie1=input("enter the movie no.1")
-movie2=input("enter the movie no.2")
-movie3=input("enter the movie no.3")
-
-movies.append(movie1)
-movies.append(movie2)
-movies.append(movie3)
-print(movies)
+student ={
+    "name": "Soumya Ranjan Sahoo" ,
+    "age":"20",
+    "class":"b-tech",
+}
+student.update({"city":"cuttack"})
+print(student)
 
